605d2dd3904b1bc7d423ff08a2124c297b2cc2d1
ChenDeYou <1780080951@qq.com>
2020-08-04 21:34:20 +0800

    把本地工程添加到文件，这是用idea第一次使用git啊

---
+++ git-test-project/src/com/dy/Gitclass.java
@@ -0,0 +1,18 @@
+package com.dy;
+
+public class Gitclass {
+
+    public static void main(String[] args) {
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+
+    }
+
+}
